05513276e4ed6945f115bb445d6c0d3add6962e0
Atilla Önver <Atillaonver@users.noreply.github.com>
2026-02-21 13:41:43 +0300

    by_text_move fonksiyonu iyilestirildi ve coklu site destegi artirildi

--- develope/bytao.php
+++ develope/bytao.php
@@ -1,4 +1,24 @@
 <?php
+if (!function_exists('by_ADM')) {
+    function by_ADM($value = null) {
+        static $by_ADM = ""; 
+        if ($value !== null) {
+            $by_ADM = $value; 
+        }
+        return $by_ADM; 
+    }
+}
+
+if (!function_exists('by_URL')) {
+    function by_URL($value = null) {
+        static $by_URL = ""; 
+        if ($value !== null) {
+            $by_URL = $value; 
+        }
+        return $by_URL; 
+    }
+}
+
 function by_move(string $image)
 {
 	if($image)
@@ -71,30 +91,7 @@ function by_cdn(string $file)
 			}else{
 				fwrite(fopen(DIR_LOGS . 'error.log', 'a'), date('Y-m-d G:i:s') . " - " .  " *** Adm Dosya yok:-" . (print_r(DIR_CDNALL.$file,TRUE)) . " \n");
 			}
-		}/*
-		else{
-			if(!is_file(DIR_CDNALL . $file))
-			{
-				$path        = '';
-				$directories = explode('/', dirname(str_replace('../', '', $file )));
-				foreach($directories as $directory)
-				{
-					if($path == '')
-					{
-						$path = $directory;
-					}
-					else
-					{
-						$path = $path . '/' . $directory;
-					}
-					if(!is_dir(DIR_CDNALL . $path))
-					{
-						@mkdir(DIR_CDNALL . $path, 0777);
-					}
-				}
-				copy(DIR_CDN.$file, DIR_CDNALL.$file);
-			}
-		}*/
+		}
 	}
 	return $file;
 }
@@ -117,6 +114,15 @@ function css_process( string $content , string $route ){
     return $content;	
 }
 
+function by_toRoute( string $content , string $route,$to = false ){
+    if($to){
+        $content = str_replace($route ,'URL_IMAGE',  $content);
+    }else{
+        $content = str_replace('URL_IMAGE', $route , $content);
+    }
+    return $content;	
+}
+
 function by_SEO (string $string, string $sub ="-"){
 	
 	$tr = ["ş","Ş","ı","ü","Ü","ö","Ö","ç","Ç","ş","Ş","ı","ğ","Ğ","İ","ö","Ö","Ç","ç","ü","Ü"];
@@ -134,45 +140,59 @@ function by_SEO (string $string, string $sub ="-"){
     return $string;
 }
 
-function icnv (string $string)
+function by_QRLink(string $url, int $size = 140,string $descriptionLong = '',string $heading_title='', string $errorCorrectionLevel = 'H', string $margin = '0', string $encoding='UTF-8')
 {
+	$QR  = '<span>';
+	$QR .='<img longdesc="'.$descriptionLong.'" crossorigin="anonymous" style="width:'.$size.'px;height:' . $size .'px;" src="'; 
+	$QR .='https://chart.googleapis.com/chart?choe='.$encoding.'&chs='.$size.'x'.$size.'&cht=qr&chld='.$errorCorrectionLevel.'|'.$margin.'&chl='.urlencode($url).'" alt="' . $heading_title . '" ></span>';
+	return $QR;
+}
+
+function icnv (string $string) {
 	return iconv('UTF-8','ASCII//TRANSLIT',str_replace(['–'], '-', $string));
 }
 
-function generateToken($length = 32)
-{
-	return bin2hex(random_bytes($length));
+function byToken($length = 32) {
+return bin2hex(random_bytes($length));
 }
 
-function by_QRLink(string $url, int $size = 140,string $descriptionLong = '',string $heading_title='', string $errorCorrectionLevel = 'H', string $margin = '0', string $encoding='UTF-8'){
-	
-		$QR  = '<span>';
-		$QR .='<img longdesc="'.$descriptionLong.'" crossorigin="anonymous" style="width:'.$size.'px;height:' . $size .'px;" src="'; 
-		$QR .='https://chart.googleapis.com/chart?choe='.$encoding.'&chs='.$size.'x'.$size.'&cht=qr&chld='.$errorCorrectionLevel.'|'.$margin.'&chl='.urlencode($url).'" alt="' . $heading_title . '" ></span>';
-		return $QR;
-	}
-	
-function by_text_move(string $text, bool $to_placeholder = true) {
+
+
+function by_text_move(string $text, bool $to_placeholder = true, $route = '') {
 	if (empty($text)) return $text;
 
 	if ($to_placeholder) {
 		// --- KAYDETME / TAŞIMA MODU ---
-		// simyaci.tr linklerini yakala -> |IMGROUTE| çevir ve dosyayı çek
-		$pattern = '/src=["\'](https:\/\/www\.simyaci\.tr\/image\/([^"\']+))["\']/';
-		
-		return preg_replace_callback($pattern, function($matches) {
-			$relative_path = $matches[2];
+		// 1. Dış kaynaklı (simyaci.tr) linkleri yakala
+		$pattern_remote = '/(src|href)=["\']https?:\/\/(www\.)?simyaci\.tr\/image\/([^"\']+)["\']/';
+		$text = preg_replace_callback($pattern_remote, function($matches) {
+			$attr = $matches[1]; // src veya href
+			$relative_path = $matches[3];
 			
 			// Resmi alt siteye kopyala (Hızır Servis)
 			by_move($relative_path);
 			
 			// Veritabanına |IMGROUTE| ile kaydet
-			return 'src="|IMGROUTE|' . $relative_path . '"';
+			return $attr . '="|IMGROUTE|' . $relative_path . '"';
 		}, $text);
+
+		// 2. Yerel tam yolları yakala (URL_IMAGE içeren)
+		if (defined('URL_IMAGE')) {
+			$local_url = preg_quote(URL_IMAGE, '/');
+			$pattern_local = '/(src|href)=["\']' . $local_url . '([^"\']+)["\']/';
+			$text = preg_replace($pattern_local, '$1="|IMGROUTE|$2"', $text);
+		}
+
+		// 3. Yerel rölatif yolları yakala (image/ ile başlayan)
+		$pattern_relative = '/(src|href)=["\']image\/([^"\']+)["\']/';
+		$text = preg_replace($pattern_relative, '$1="|IMGROUTE|$2"', $text);
+
+		return $text;
 		
 	} else {
 		// --- GÖSTERME / YAYINLAMA MODU ---
 		// |IMGROUTE| gördüğünde yerel resim yoluna çevir
-		return str_replace('|IMGROUTE|', 'image/', $text);
+		$route = $route ?: (defined('URL_IMAGE') ? URL_IMAGE : 'image/');
+		return str_replace('|IMGROUTE|', $route, $text);
 	}
 }
