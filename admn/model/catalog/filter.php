2a977a7c414778f7469dd0c91635a533bde71ce3
Atilla Önver <Atillaonver@users.noreply.github.com>
2026-03-12 07:52:09 +0300

    WIP: Filters

--- admn/model/catalog/filter.php
+++ admn/model/catalog/filter.php
@@ -121,7 +121,7 @@ class Filter extends \Opencart\System\Engine\Model {
 
 	public function getFilter(int $filter_id): array {
 		$store_id = isset($this->session->data['store_id'])?$this->session->data['store_id']:0;	
-		$query = $this->db->query("SELECT *, (SELECT `name` FROM `" . DB_PREFIX . "filter_group_description` fgd WHERE f.`filter_group_id` = fgd.`filter_group_id` AND fgd.`language_id` = '" . (int)$this->config->get('config_language_id') . "') AS `group` FROM `" . DB_PREFIX . "filter` f LEFT JOIN `" . DB_PREFIX . "filter_description` fd ON (f.`filter_id` = fd.`filter_id`) WHERE f.`filter_id` = '" . (int)$filter_id . "' AND fd.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND f.`store_id` = '" . (int)$store_id . "'");
+		$query = $this->db->query("SELECT *, (SELECT `name` FROM `" . DB_PREFIX . "filter_group_description` fgd WHERE f.`filter_group_id` = fgd.`filter_group_id` AND fgd.`language_id` = '" . (int)$this->config->get('config_language_id') . "') AS `group` FROM `" . DB_PREFIX . "filter` f LEFT JOIN `" . DB_PREFIX . "filter_description` fd ON (f.`filter_id` = fd.`filter_id`) LEFT JOIN `" . DB_PREFIX . "filter_group` fg ON (f.`filter_group_id` = fg.`filter_group_id`) WHERE f.`filter_id` = '" . (int)$filter_id . "' AND fd.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND fg.`store_id` = '" . (int)$store_id . "'");
 
 		return $query->row;
 	}
--- admn/model/catalog/product.php
+++ admn/model/catalog/product.php
@@ -219,9 +219,8 @@ class Product extends \Opencart\System\Engine\Model {
 
 
 		// Categories
-		$this->db->query("DELETE FROM `" . DB_PREFIX . "product_to_category` WHERE `product_id` = '" . (int)$product_id . "'");
-
 		if (isset($data['product_category'])) {
+			$this->db->query("DELETE FROM `" . DB_PREFIX . "product_to_category` WHERE `product_id` = '" . (int)$product_id . "'");
 			foreach ($data['product_category'] as $category_id) {
 				$this->db->query("INSERT INTO `" . DB_PREFIX . "product_to_category` SET `product_id` = '" . (int)$product_id . "', `category_id` = '" . (int)$category_id . "'");
 			}
