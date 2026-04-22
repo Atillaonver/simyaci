e3e903df3e4145106ed9bf7fbbb8aa88c4f445f7
Atilla Önver <Atillaonver@users.noreply.github.com>
2026-04-22 20:25:11 +0300

    WIP: Admin Clear Cache

--- extension/opencart/catalog/model/module/bestseller.php
+++ extension/opencart/catalog/model/module/bestseller.php
@@ -2,7 +2,7 @@
 namespace Opencart\Catalog\Model\Extension\Opencart\Module;
 class Bestseller extends \Opencart\System\Engine\Model {
 	public function getBestSeller(int $limit): array {
-		$product_data = (array)$this->cache->get('product.bestseller.' . (int)$this->config->get('config_language_id') . '.' . (int)$this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . (int)$limit);
+		$product_data = (array)$this->cache->get('product.bestseller.' . (int)$this->config->get('config_language_id') . '.' . (int)$this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . (int)$limit,$this->config->get('config_store_id'));
 
 		if (!$product_data) {
 			$query = $this->db->query("SELECT op.`product_id`, SUM(op.`quantity`) AS `total` FROM `" . DB_PREFIX . "order_product` op LEFT JOIN `" . DB_PREFIX . "order` o ON (op.`order_id` = o.`order_id`) LEFT JOIN `" . DB_PREFIX . "product` p ON (op.`product_id` = p.`product_id`) LEFT JOIN `" . DB_PREFIX . "product_to_store` p2s ON (p.`product_id` = p2s.`product_id`) WHERE o.`order_status_id` > '0' AND p.`status` = '1' AND p.`date_available` <= NOW() AND p2s.`store_id` = '" . (int)$this->config->get('config_store_id') . "' GROUP BY op.`product_id` ORDER BY `total` DESC LIMIT " . (int)$limit);
@@ -11,7 +11,7 @@ class Bestseller extends \Opencart\System\Engine\Model {
 				$product_data[$result['product_id']] = $this->model_catalog_product->getProduct($result['product_id']);
 			}
 
-			$this->cache->set('product.bestseller.' . (int)$this->config->get('config_language_id') . '.' . (int)$this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . (int)$limit, $product_data);
+			$this->cache->set('product.bestseller.' . (int)$this->config->get('config_language_id') . '.' . (int)$this->config->get('config_store_id') . '.' . $this->config->get('config_customer_group_id') . '.' . (int)$limit, $product_data,$this->config->get('config_store_id'));
 		}
 
 		return $product_data;
