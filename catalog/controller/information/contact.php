22aac0dd65a1fe5a132df67710dccec8e0c9701a
Atilla Önver <Atillaonver@users.noreply.github.com>
2026-05-04 17:40:31 +0300

    WIP: Admin information contact

--- catalog/controller/information/contact.php
+++ catalog/controller/information/contact.php
@@ -43,6 +43,7 @@ class Contact extends \Opencart\System\Engine\Controller {
 		$data['geocode'] = $this->config->get('config_geocode');
 		$data['geocode_hl'] = $this->config->get('config_language');
 		$data['telephone'] = $this->config->get('config_telephone');
+		$data['gsm'] = $this->config->get('config_gsm');
 		$data['open'] = nl2br($this->config->get('config_open'));
 		$data['comment'] = nl2br($this->config->get('config_comment'));
 
